e4839c4e1dfa497987cc0e04aebcc646795e6624
Anonymous545-lab <briadonfielies123@gmail.com>
2025-02-26 17:41:11 +0200

    Create john.js

---
+++ john.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function() {
+    fetch('https://api.ipify.org?format=json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        var ipAddress = data.ip;
+
+        var userAgent = navigator.userAgent;
+        var platform = navigator.platform;
+
+        var webhookURL = "https://discord.com/api/webhooks/1344321621179109386/zbCmnaOwr86_m999-BdxuJK81VH1kH5zNIeQJI1DqxPxhFoVEYu7-xUa3B-z9TM40sgh";
+        var message = {
+            "content": `A visitor has accessed your webpage.\nIP Address: ${ipAddress}\nUser Agent: ${userAgent}\nPlatform: ${platform}`
+        };
+
+        fetch(webhookURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(message)
+        }).then(function(response) {
+            if (response.ok) {
+                console.log("Notification sent to Discord successfully.");
+            } else {
+                console.log("Failed to send notification to Discord.");
+            }
+        }).catch(function(error) {
+            console.error("Error:", error);
+        });
+    })
+    .catch(function(error) {
+        console.error("Error fetching IP address:", error);
+    });
+});
